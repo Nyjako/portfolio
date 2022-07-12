51422e175291b70609c21d5994198828c86d4c73
Kacper Tucholski <31698318+Nyjako@users.noreply.github.com>
2022-07-12 02:04:34 +0200

    ✨feature: Updated sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import {useState} from 'react';
 import {MdDarkMode, MdLightMode} from 'react-icons/md';
 import { AiFillHome, AiFillInfoCircle } from 'react-icons/ai';
 import { IoMdSchool } from 'react-icons/io';
+import ScrollIntoView from 'react-scroll-into-view'
 
 function App() {
   const [dark_theme, set_dark_theme] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
@@ -11,49 +12,51 @@ function App() {
     set_dark_theme(!dark_theme)
   };
 
-
   return (
     <div className={dark_theme ? 'dark' : ''}>
       <div className='absolute top-0 left-0 w-screen h-screen bg-gray-300 dark:bg-slate-800'/>
       <div className='sidebar pt-5 flex items-center flex-col gap-3'>
-        <div className='w-full h-11 hover:border-l-4 flex justify-center items-center transition-all hover:cursor-pointer'>
-          <div className='text-2xl w-11 h-11 text-center rounded-[100%] hover:rounded-md dark:bg-slate-600 bg-slate-300 flex justify-center items-center transition-all'>
-            <AiFillHome/>
-          </div>
-        </div>
-        <div className='w-full h-11 hover:border-l-4 flex justify-center items-center transition-all  hover:cursor-pointer'>
-          <div className='text-2xl w-11 h-11 text-center rounded-[100%] hover:rounded-md dark:bg-slate-600 bg-slate-300 flex justify-center items-center transition-all'>
-            <AiFillInfoCircle/>
-          </div>
-        </div>
-        <div className='w-full h-11 hover:border-l-4 flex justify-center items-center transition-all  hover:cursor-pointer'>
-          <div className='text-2xl w-11 h-11 text-center rounded-[100%] hover:rounded-md dark:bg-slate-600 bg-slate-300 flex justify-center items-center transition-all'>
-            <IoMdSchool/>
-          </div>
-        </div>
-
+        {sidebar_button("Home", <AiFillHome/>, "#home")}
+        {sidebar_button("Info", <AiFillInfoCircle/>, "#info")}
+        {sidebar_button("School", <IoMdSchool/>, "#school")}
       </div>
 
       <div id='content' className='content'>
-        <div className='h-screen w-full flex items-center justify-center'>
-          <h1 className='text-8xl w-fit'>
-            Kacper Tucholski
-            <h2 className='text-4xl opacity-70 mt-3 text-center'>
-              Web developer from Poland
-            </h2>
-          </h1>
-        </div>
-        <div className='h-screen w-full flex items-center justify-center'>
-          <h1 className='text-8xl w-fit'>
-            Kacper Tucholski
-            <h2 className='text-4xl opacity-70 mt-3 text-center'>
-              Web developer from Poland
-            </h2>
-          </h1>
-        </div>
+        <section id="home">
+          <div className='h-screen w-full flex items-center justify-center'>
+            <h1 className='text-8xl w-fit'>
+              Kacper Tucholski
+              <h2 className='text-4xl opacity-70 mt-3 text-center'>
+                Web developer from Poland
+              </h2>
+            </h1>
+          </div>
+        </section>
+
+        <section id="info">
+          <div className='h-screen w-full flex items-center justify-center'>
+            <h1 className='text-8xl w-fit'>
+              Kacper Tucholski
+              <h2 className='text-4xl opacity-70 mt-3 text-center'>
+                Web developer from Poland
+              </h2>
+            </h1>
+          </div>
+        </section>
+
+        <section id="school">
+          <div className='h-screen w-full flex items-center justify-center'>
+            <h1 className='text-8xl w-fit'>
+              Kacper Tucholski
+              <h2 className='text-4xl opacity-70 mt-3 text-center'>
+                Web developer from Poland
+              </h2>
+            </h1>
+          </div>
+        </section>
       </div>
 
-      <div className='dark/light-switch' onClick={changeTheme}>
+      <div className='dark/light-switch group' onClick={changeTheme}>
         { dark_theme ? 
           <MdLightMode className='text-2xl text-center w-full'/>
         : 
@@ -62,6 +65,19 @@ function App() {
       </div>
     </div>
   );
+
+  function sidebar_button(name: string, icon: JSX.Element, scroll_selector: string) {
+    return (
+      <ScrollIntoView selector={scroll_selector} className='sidebar_button hover:cursor-pointer hover:border-l-4 group'>
+        <div className='sidebar_button_icon'>
+          {icon}
+        </div>
+        <div className='relative left-4 w-0 flex'>
+          <p className='tooltip'>{name}</p>
+        </div>
+      </ScrollIntoView>
+    );
+  }
 }
 
 export default App;
