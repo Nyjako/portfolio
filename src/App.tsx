5e0d03cd53780a4c4df6fec94df1ea166846f629
Kacper Tucholski <31698318+Nyjako@users.noreply.github.com>
2022-07-12 20:01:03 +0200

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {useState} from 'react';
-import {MdDarkMode, MdLightMode} from 'react-icons/md';
+import {MdDarkMode, MdLightMode, MdOutlineConnectWithoutContact} from 'react-icons/md';
 import { AiFillHome, AiFillInfoCircle } from 'react-icons/ai';
 import { IoMdSchool } from 'react-icons/io';
+import { FaLaptopCode } from 'react-icons/fa'
 import ScrollIntoView from 'react-scroll-into-view'
 
 function App() {
@@ -18,15 +19,17 @@ function App() {
       <div className='sidebar pt-5 flex items-center flex-col gap-3'>
         {sidebar_button("Home", <AiFillHome/>, "#home")}
         {sidebar_button("Info", <AiFillInfoCircle/>, "#info")}
+        {sidebar_button("Technologies", <FaLaptopCode/>, "#technologies")}
         {sidebar_button("School", <IoMdSchool/>, "#school")}
+        {sidebar_button("Contact", <MdOutlineConnectWithoutContact/>, "#contact")}
       </div>
 
       <div id='content' className='content'>
         <section id="home">
           <div className='h-screen w-full flex items-center justify-center'>
-            <h1 className='text-8xl w-fit'>
+            <h1 className='md:text-8xl sm:text-4xl text-2xl w-fit'>
               Kacper Tucholski
-              <h2 className='text-4xl opacity-70 mt-3 text-center'>
+              <h2 className='md:text-4xl sm:text-2xl text-xl opacity-70 mt-3 text-center'>
                 Web developer from Poland
               </h2>
             </h1>
@@ -34,24 +37,57 @@ function App() {
         </section>
 
         <section id="info">
-          <div className='h-screen w-full flex items-center justify-center'>
-            <h1 className='text-8xl w-fit'>
-              Kacper Tucholski
-              <h2 className='text-4xl opacity-70 mt-3 text-center'>
-                Web developer from Poland
-              </h2>
-            </h1>
+          <div className='min-h-screen w-full flex justify-center'>
+            <h2 className='text-8xl w-fit'>
+              Info
+            </h2>
+          </div>
+        </section>
+
+        <section id="technologies">
+          <div className='min-h-screen w-full flex items-center flex-col'>
+            <h2 className='text-8xl w-fit'>
+              Technologies
+            </h2>
+            <div className='w-full flex justify-center'>
+              <table>
+                <tr>
+                  <th colSpan={2}>Languages</th>
+                </tr>
+                <tr>
+                  <th>Language</th>
+                  <th>Level</th>
+                </tr>
+                <tr>
+                  <td>C++</td>
+                  <td>●●○○</td>
+                </tr>
+                <tr>
+                  <td>C++</td>
+                  <td>●●○○</td>
+                </tr>
+                <tr>
+                  <td>C++</td>
+                  <td>●●○○</td>
+                </tr>
+              </table>
+            </div>
           </div>
         </section>
 
         <section id="school">
-          <div className='h-screen w-full flex items-center justify-center'>
-            <h1 className='text-8xl w-fit'>
-              Kacper Tucholski
-              <h2 className='text-4xl opacity-70 mt-3 text-center'>
-                Web developer from Poland
-              </h2>
-            </h1>
+          <div className='min-h-screen w-full flex items-center justify-center'>
+            <h2 className='text-8xl w-fit'>
+              School
+            </h2>
+          </div>
+        </section>
+
+        <section id="contact">
+          <div className='min-h-screen w-full flex items-center justify-center'>
+            <h2 className='text-8xl w-fit'>
+              Contact
+            </h2>
           </div>
         </section>
       </div>
