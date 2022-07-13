38ab8614a40efae0dcf18ea7172ddacb94dcbcfb
Kacper "Nyjako" Tucholski <31698318+Nyjako@users.noreply.github.com>
2022-07-13 21:13:18 +0200

    ✨feature: Added blur for better readability

--- src/App.tsx
+++ src/App.tsx
@@ -38,39 +38,45 @@ function App() {
       <div id='content' className='content'>
         <section id="home">
           <div className='h-screen w-full flex items-center justify-center text-center'>
-            <h1 className='head_text w-fit p-10'>
-              Kacper Tucholski
-              <h2 className='mid_text opacity-70 mt-3 text-center'>
-                Web developer from Poland
-              </h2>
-            </h1>
+            <div className='bg_blur'>
+              <h1 className='head_text w-fit p-10'>
+                Kacper Tucholski
+                <h2 className='mid_text opacity-70 mt-3 text-center'>
+                  Web developer from Poland
+                </h2>
+              </h1>
+            </div>
           </div>
         </section>
 
         <section id="about_me">
           <div className='min-h-screen w-full flex justify-center items-center flex-col md:gap-10 gap-0'>
-            <h2 className='head_text w-fit'>
-              About me
-            </h2>
-            <div className='w-full p-10 flex md:flex-row flex-col md:gap-16 gap-5 justify-center items-center'>
-              <div className='flex flex-col gap-5 md:text-2xl text-xl md:text-right text-center'>
-                <p>I'm web developer from Poland. I have passion for creating something from nothing</p>
-                <p>In my spare time, I like to develop my programming skills, play games and watch anime</p>
-                <p>My dream is to one day make my own game</p>
+            <div className='bg_blur'>
+              <h2 className='head_text w-fit'>
+                About me
+              </h2>
+              <div className='w-full p-10 flex md:flex-row flex-col md:gap-16 gap-5 justify-center items-center'>
+                <div className='flex flex-col gap-5 md:text-2xl text-xl md:text-right text-center'>
+                  <p>I'm web developer from Poland. I have passion for creating something from nothing</p>
+                  <p>In my spare time, I like to develop my programming skills, play games and watch anime</p>
+                  <p>My dream is to one day make my own game</p>
+                </div>
+                <img src="prof.png" alt="" srcSet="" className='rounded-lg shadow-xl md:w-[256px] max-w-full aspect-square' />
               </div>
-              <img src="prof.png" alt="" srcSet="" className='rounded-lg shadow-xl md:w-[256px] max-w-full aspect-square mb-32 md:mb-0' />
             </div>
           </div>
         </section>
 
         <section id="skills">
           <div className='min-h-screen w-full flex md:justify-center items-center flex-col md:gap-10 gap-5'>
-            <h2 className='head_text h-fit text-center'>
-              Skills
-            </h2>
-            <div className='w-full flex justify-center items-center gap-5'>
-              <div className='flex w-fit h-fit tab:gap-24 gap-10 tab:flex-row flex-col'>
-                <Skills/>
+            <div className='bg_blur'>
+              <h2 className='head_text h-fit text-center mb-5'>
+                Skills
+              </h2>
+              <div className='w-full flex justify-center items-center gap-5'>
+                <div className='flex w-fit h-fit tab:gap-24 gap-10 tab:flex-row flex-col'>
+                  <Skills/>
+                </div>
               </div>
             </div>
           </div>
@@ -78,48 +84,52 @@ function App() {
 
         <section id="school">
           <div className='min-h-screen w-full flex items-center justify-center flex-col'>
-            <h2 className='head_text w-fit mb-10'>
-              School
-            </h2>
-            <p className='mid_text text-center w-fit'>
-              Państwowa Wyższa Szkoła Zawodowa w Nysie
-            </p>
-            <p className='sm_text w-fit opacity-70'>
-              2018 - Present 
-            </p>
+            <div className='bg_blur'>
+              <h2 className='head_text w-fit mb-10'>
+                School
+              </h2>
+              <p className='mid_text text-center w-fit'>
+                Państwowa Wyższa Szkoła Zawodowa w Nysie
+              </p>
+              <p className='sm_text w-fit opacity-70'>
+                2018 - Present 
+              </p>
+            </div>
           </div>
         </section>
 
         <section id="contact">
           <div className='min-h-screen w-full flex items-center justify-center flex-col md:gap-10 gap-5'>
-            <h2 className='head_text w-fit'>
-              Contact & Social
-            </h2>
-            <div className='flex gap-5 flex-col md:flex-row items-center md:items-start'>
-              <a href="mailto:kacper.tucholski.kt@gmail.com">
-                <button className='social_button'>
-                  <AiFillMail className='text-2xl'/>
-                  Get in Touch
-                </button>
-              </a>
-              <a href="https://github.com/Nyjako" target="_blank" rel="noopener noreferrer">
-                <button className='social_button'>
-                  <AiFillGithub className='text-2xl'/>
-                  Github
-                </button>
-              </a>
-              <a href="https://www.linkedin.com/in/nyjako/" target="_blank" rel="noopener noreferrer">
-                <button className='social_button'>
-                  <AiFillLinkedin className='text-2xl'/>
-                  LinkedIn
-                </button>
-              </a>
+            <div className='bg_blur'>
+              <h2 className='head_text w-fit md:mb-10 mb-5'>
+                Contact & Social
+              </h2>
+              <div className='flex gap-5 flex-col md:flex-row items-center md:items-start'>
+                <a href="mailto:kacper.tucholski.kt@gmail.com">
+                  <button className='social_button'>
+                    <AiFillMail className='text-2xl'/>
+                    Get in Touch
+                  </button>
+                </a>
+                <a href="https://github.com/Nyjako" target="_blank" rel="noopener noreferrer">
+                  <button className='social_button'>
+                    <AiFillGithub className='text-2xl'/>
+                    Github
+                  </button>
+                </a>
+                <a href="https://www.linkedin.com/in/nyjako/" target="_blank" rel="noopener noreferrer">
+                  <button className='social_button'>
+                    <AiFillLinkedin className='text-2xl'/>
+                    LinkedIn
+                  </button>
+                </a>
+              </div>
             </div>
           </div>
         </section>
         <section id="info">
           <div className='min-h-screen w-full flex items-center justify-center flex-col md:gap-10 gap-5'>
-            <div className='dark:bg-black/20 bg-white/60 rounded-lg shadow-md backdrop-blur-2xl p-10 flex items-center flex-col'>
+            <div className='bg_blur'>
             <h2 className='head_text w-fit'>
               Info
             </h2>
